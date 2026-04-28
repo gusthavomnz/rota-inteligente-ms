34b60fab653cd830a3e038e5333ca55879cab0f4
Gusthavo <gusthavogp123@gmail.com>
2026-04-28 00:04:17 -0300

    feat: criação das classes models

---
+++ rota-inteligente/src/main/java/models/Product.java
@@ -0,0 +1,22 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "weight", nullable = false, precision = 10, scale = 3)
+    private BigDecimal weight;
+}
